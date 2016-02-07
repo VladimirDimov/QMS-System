0579388d426f00dbf6ef7cf6b115d48cec5b4070
VladimirDimov <vdimov1124@gmail.com>
2016-02-07 18:11:42 +0200

    departments services added

---
+++ Source/QMS.Services/DepartmentsServices.cs
@@ -0,0 +1,45 @@
+﻿using QMS.Data;
+using QMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QMS.Services
+{
+    class DepartmentsServices
+    {
+        private IQmsData data;
+
+        public DepartmentsServices(IQmsData data)
+        {
+            this.data = data;
+        }
+
+        public IQueryable<Department> All()
+        {
+            return this.data.Departments.All();
+        }
+
+        public Department GetById(int id)
+        {
+            return this.data.Departments.GetById(id);
+        }
+
+        public Department Create(string name, string description, int divisionId)
+        {
+            var departmentToAdd = new Department
+            {
+                Name = name,
+                Description = description,
+                DivisionId = divisionId
+            };
+
+            this.data.Departments.Add(departmentToAdd);
+            this.data.SaveChanges();
+
+            return departmentToAdd;
+        }
+    }
+}
---
+++ Source/QMS.Web/Areas/Admin/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QMS.Web.Areas.Admin.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Admin/Departments
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
