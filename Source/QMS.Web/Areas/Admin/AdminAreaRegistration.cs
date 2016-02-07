8d4c83b81ac62c255bc2b88fb8df8e4f7eb8e11d
VladimirDimov <vdimov1124@gmail.com>
2016-02-07 14:06:03 +0200

    Admin area registration modified

--- Source/QMS.Web/Areas/Admin/AdminAreaRegistration.cs
+++ Source/QMS.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -2,17 +2,17 @@
 
 namespace QMS.Web.Areas.Admin
 {
-    public class AdminAreaRegistration : AreaRegistration 
+    public class AdminAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Admin";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "Admin_default",
