1b12b1356b1d705ed3a299eaa3b7d6f9a7251c53
VladimirDimov <vdimov1124@gmail.com>
2016-02-08 23:41:27 +0200

    models area minor changes

--- Source/QMS.Models/Area.cs
+++ Source/QMS.Models/Area.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QMS.Models
+﻿namespace QMS.Models
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     public class Area
     {
         private ICollection<Record> records;
