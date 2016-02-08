2c6d609d50cc45234b2baebfc13ee9ddb9e4903b
VladimirDimov <vdimov1124@gmail.com>
2016-02-08 23:54:55 +0200

    Records area created

---
+++ Source/QMS.Web/Areas/Private/PrivateAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace QMS.Web.Areas.Private
+{
+    public class PrivateAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Private";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Private_default",
+                "Private/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
