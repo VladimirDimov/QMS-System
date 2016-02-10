a4c49ba71a168eaec4caa82aa45ff61b2d996701
VladimirDimov <vdimov1124@gmail.com>
2016-02-10 14:36:25 +0200

    register form style fixed

--- Source/QMS.Web.Models/AccountViewModels.cs
+++ Source/QMS.Web.Models/AccountViewModels.cs
@@ -76,12 +76,14 @@ namespace QMS.Web.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [UIHint("Password")]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [UIHint("Password")]
         public string ConfirmPassword { get; set; }
 
         [MaxLength(50)]
