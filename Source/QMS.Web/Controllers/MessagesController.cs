310cda017137b2e6ae5db6e3d5d878caed3ee791
VladimirDimov <vdimov1124@gmail.com>
2016-02-16 00:41:40 +0200

    SignalR test logic added

---
+++ Source/QMS.Web/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QMS.Web.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Messages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/QMS.Web/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿namespace QMS.Web.Hubs
+{
+    using Microsoft.AspNet.SignalR;
+
+    public class ChatHub : Hub
+    {
+        public void Send(string message)
+        {
+            var senderUsername = Context.User.Identity.Name;
+            Clients.All.broadcastMessage(senderUsername, message);
+        }
+    }
+}
