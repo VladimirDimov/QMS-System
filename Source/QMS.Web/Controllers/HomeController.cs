dbc4d20cf751f6072198312848c3954334bc370e
VladimirDimov <vdimov1124@gmail.com>
2016-02-15 12:00:10 +0200

    Areas public logic added

--- Source/QMS.Web/Controllers/HomeController.cs
+++ Source/QMS.Web/Controllers/HomeController.cs
@@ -10,21 +10,21 @@ namespace QMS.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View("About");
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return View("Contact");
         }
     }
 }
